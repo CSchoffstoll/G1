1a4af631417b95b4a631716561709bba3d4930c5
Sophie Omar <somar12@saddleback.edu>
2025-09-06 20:54:12 -0700

    Actual Prototype Commit

---
+++ search.cpp
@@ -0,0 +1,7 @@
+#include "search.h"
+
+//Define Function
+
+int linearSearch(const std::vector<std::string>& list, const std::string& key){
+	// implement the search function!!!
+}
---
+++ search.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+//Linear Search Function Prototype
+
+int linearSearch(const std::vector<std::string>&list, const std::string& key); 
+
+#endif
