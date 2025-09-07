910663b5e3707676de0846c4ca81b09f73164c62
Schoffstoll <cschoffstoll0@ivc.edu>
2025-09-06 23:26:20 -0700

    main.cpp intro progress

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
+#include <search.h>
+#include <vector>
+#include <ctype>
+#include <string>
 
-cout << "TEST";
+using namespace std;
+
+int main() {
+	char choice ='';  //user entered decision -> while loop
+	
+
+	//setup for user to read and make decision
+	cout << "LINEAR SEARCH\n\n" 
+		  << "<S>earch\n"
+		  << "<Q>uit\n\n"
+		  << "Choice: ";
+	cin >> choice; //accept users choice
+
+	choice = tolower(choice);
+
+	do {
+		string search
+		cout << "Search: 
+	} while (choice != q);
