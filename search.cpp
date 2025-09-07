f272c1770edbc0580f04b09c24472ba6ef5df8e8
Sophie Omar <somar12@saddleback.edu>
2025-09-06 21:03:25 -0700

    Added Linear Search Definition

--- search.cpp
+++ search.cpp
@@ -3,5 +3,10 @@
 //Define Function
 
 int linearSearch(const std::vector<std::string>& list, const std::string& key){
-	// implement the search function!!!
+	//Loop through vector for key (basic vector search from notes)
+	for (std:: size_t i = 0; i <list.size(); i++){
+		if(list[i] == key){
+		return static_cast<int>(i);
+		}
+	} return -1; //returns -1 if the value is not present
 }
